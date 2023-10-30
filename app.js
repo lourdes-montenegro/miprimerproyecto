aee1f4c7ba8a41eeaf812202f2b35a161b82eed8
lourdes-montenegro <lourdesmontenegro017@gmail.com>
2023-10-30 14:22:51 -0300

    Prueba

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 
-let app = express
-console.log(express);
+const app = express()
+// app.length('/', (req, res) => {
+//     res.send('Hola mundo :)')
+// })
+app.listen(3000, () => 
+    console.log('servidor corriendo'))
+
+app.get('/', function(req, res) {
+    res.send('Bienvenidos al sitio')
+
+})
