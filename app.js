8fe058f6a3f92ac95bae11d4f0b6ac1b59ad86a0
lourdes-montenegro <lourdesmontenegro017@gmail.com>
2023-10-31 14:00:45 -0300

    primeras lineas en HTML

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-
+const path = require('path')
 const app = express()
 // app.length('/', (req, res) => {
 //     res.send('Hola mundo :)')
 // })
-app.listen(3000, () => {
+const port = 3000
+app.listen(port, () => {
     console.log('servidor corriendo')
-})  
+})
     ;
 app.get('/', (req, res) => {
-    res.sendFile('./views/home.html')
+    let htmlPath = path.resolve(__dirname, './views/home.html')
+    res.sendFile(htmlPath)
 
 })
