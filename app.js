d56cb2a1761c4c9dc613a2ffe56d85fed0103ef2
lourdes-montenegro <lourdesmontenegro017@gmail.com>
2023-10-31 20:55:05 -0300

    fronted de mercado liebre (html)

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path')
 const app = express()
-// app.length('/', (req, res) => {
-//     res.send('Hola mundo :)')
-// })
+
+const publicPath = path.resolve(__dirname, './public')
+
+    app.use(express.static(publicPath));
+
 const port = 3000
+
 app.listen(port, () => {
     console.log('servidor corriendo')
 })
