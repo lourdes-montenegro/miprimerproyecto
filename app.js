28e5c3f6bc2bb8138da1120d4515637701d854af
lourdes-montenegro <lourdesmontenegro017@gmail.com>
2023-11-01 11:53:36 -0300

    archivo html modificaciones

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path')
 const app = express()
 
-const publicPath = path.resolve(__dirname, '/public')
+const publicPath = path.resolve(__dirname, './public')
 
     app.use(express.static(publicPath));
 
+const port = 4000
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('servidor corriendo')
 })
     ;
