2fde3c872d0acf1a2ec3199489464a3aa70057bf
lourdes-montenegro <lourdesmontenegro017@gmail.com>
2023-10-31 22:19:12 -0300

    actualizando

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const app = express()
 
-const publicPath = path.resolve(__dirname, './public')
+const publicPath = path.resolve(__dirname, '/public')
 
     app.use(express.static(publicPath));
 
@@ -13,7 +13,7 @@ app.listen(3000, () => {
 })
     ;
 app.get('/', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/index.html')
+    let htmlPath = path.resolve(__dirname, './views/home.html')
     res.sendFile(htmlPath) 
 
 })
