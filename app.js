0fe5e19cedbb1e74131fb68247b4cddc807b485c
lourdes-montenegro <lourdesmontenegro017@gmail.com>
2024-04-12 15:58:23 -0300

    renderizando las vistas

--- app.js
+++ app.js
@@ -5,16 +5,23 @@ const app = express()
 const publicPath = path.resolve(__dirname, './public')
 
     app.use(express.static(publicPath));
-
+app.set('view engine', 'ejs');
+app.set('view', './views')
 const port = process.env.PORT || 3001
 app.listen(port, () => {
-    console.log('servidor corriendo')
+    console.log('servidor corriendo en http://localhost:3001/ ')
 })
     ;
 app.get('/', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/home.html')
+    let htmlPath = path.resolve(__dirname, './views/home.ejs')
     res.sendFile(htmlPath) 
+})
+app.get('/', function(req, res) {
+    res.render('./views/home'); // Esto renderizará home.ejs
+});
 
+app.get('/', (req, res) => {
+    res.render('index')
 })
 app.get('/register',(req, res) => 
 res.sendFile(path.resolve(__dirname, './views/register.html'))
@@ -23,12 +30,3 @@ res.sendFile(path.resolve(__dirname, './views/register.html'))
 app.get('/login',(req, res) => 
 res.sendFile(path.resolve(__dirname, './views/login.html'))
 )
-
-
-// 
-// 
-// 
-// 
-// 
-// 
-
